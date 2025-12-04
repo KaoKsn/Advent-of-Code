fe52f381c4ef1a524a7d0e7153fdd72e974db3f6
Mayura <mayurakrishna.25@proton.me>
2025-12-04 23:07:29 +0530

    Day 3 - task 1, brute forcing method

---
+++ Day 3/brute_forced.py
@@ -0,0 +1,20 @@
+def lcomb(bank):
+    largest = int(bank[0:2])
+    # 
+    l = len(bank)
+    for i, f in enumerate(bank[0:l-1]):
+        for s in bank[i+1:]:
+            if largest < int(f + s):
+                largest = int(f + s)
+    return largest
+
+def main():
+    t_joltage = 0
+    with open("input.txt", "r") as file:
+        for line in file:
+            t_joltage += lcomb(line.strip())
+    print(t_joltage)
+    return 0
+
+if __name__ == "__main__":
+    main()
